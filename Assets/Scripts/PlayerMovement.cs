fddf85d3ebe7ca8c1ff47472dffb70bf4f4f8f52
dastasoft <45243062+dastasoft@users.noreply.github.com>
2020-12-23 12:23:47 +0100

    applied force to the player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    // We marked this as "Fixed"Update because we are using it to mess with physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis
+    }
+}
