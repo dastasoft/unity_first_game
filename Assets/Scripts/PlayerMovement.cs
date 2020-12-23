38cd61e13331d1d9a4efa75e7debb6a89857f76f
dastasoft <45243062+dastasoft@users.noreply.github.com>
2020-12-23 12:56:10 +0100

    camera following the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,23 @@ public class PlayerMovement : MonoBehaviour
     // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
-    // We marked this as "Fixed"Update because we are using it to mess with physics.
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as "Fixed"Update because we are using it to mess with physics
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis
+        // Add a forward force to the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
