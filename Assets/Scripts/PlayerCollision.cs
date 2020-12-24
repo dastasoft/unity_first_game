f5ec7ab37997f71259b92ca3c3c2dab5e7f8bf74
dastasoft <45243062+dastasoft@users.noreply.github.com>
2020-12-24 12:31:18 +0100

    endgame

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,12 +2,17 @@
 
 public class PlayerCollision : MonoBehaviour
 {
-    public PlayerMovement movement;
+    public PlayerMovement movement; // A reference to our PlayerMovement script
+
+    // This function runs when we hit another object
+    // We get information about the collision and call it "collisionInfo"
     void OnCollisionEnter(Collision collisionInfo)
     {
+        // We check if the object we collided with has a tag called "Obastacle"
         if (collisionInfo.collider.tag == "Obstacle")
         {
-            movement.enabled = false;
+            movement.enabled = false; // Disable the players movement.
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    private float deadLevel = -1f;
 
     // We marked this as "Fixed"Update because we are using it to mess with physics
     void FixedUpdate()
@@ -14,14 +15,21 @@ public class PlayerMovement : MonoBehaviour
         // Add a forward force to the z-axis
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d")) // If the player is pressing the "d" key
         {
+            // Add a force to the right
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a")) // If the player is pressing the "a" key
         {
+            // Add a force to the left
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < deadLevel)
+        {
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
