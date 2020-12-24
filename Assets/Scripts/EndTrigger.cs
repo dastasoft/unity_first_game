e9535c0a7e4d594bc50bc8a86543436a06de050a
dastasoft <45243062+dastasoft@users.noreply.github.com>
2020-12-24 13:02:58 +0100

    game win

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            gameManager.CompleteLevel();
+        }
+    }
+}
