12bc52c94c518624e20e5c4676094597c2f89c0b
dastasoft <dastasoft@gmail.com>
2020-12-27 12:01:02 +0100

    added slow motion effect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,48 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
-    public float restartDelay = 1f;
     public GameObject completeLevelUI;
+    public float slowness = 10f;
+
     public void CompleteLevel()
     {
         completeLevelUI.SetActive(true);
     }
+
     public void EndGame()
     {
         if (!gameHasEnded)
         {
             gameHasEnded = true;
-            Invoke("Restart", restartDelay);
+            StartCoroutine(RestartLevel());
         }
     }
 
-    void Restart()
+
+    void SlowTimeSpeed()
     {
+        Time.timeScale = 1f / slowness;
+        Time.fixedDeltaTime /= slowness;
+    }
+
+    void RestoreTimeSpeed()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime *= slowness;
+    }
+
+    IEnumerator RestartLevel()
+    {
+        SlowTimeSpeed();
+
+        yield return new WaitForSeconds(1f / slowness);
+
+        RestoreTimeSpeed();
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
